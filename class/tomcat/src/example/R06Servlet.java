fa1609f0558c8f0f9e5300e93f8afe2e9630a277
murasame29 <oogiriminister@gmail.com>
2024-09-19 10:46:39 +0900

    kadai6

--- class/tomcat/src/example/R06Servlet.java
+++ class/tomcat/src/example/R06Servlet.java
@@ -1,110 +1,157 @@
 package myServlet;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.ResultSet;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-// import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
+import myBean.UserBean;
 
+//import com.mysql.jdbc.Connection;
+//import com.mysql.jdbc.Statement;
 
 @WebServlet(urlPatterns = {"/r06"})
 public class R06Servlet 
 	extends HttpServlet
 {
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		funcServlet(request,response, "get");
+	protected void doGet(
+			HttpServletRequest req,
+			HttpServletResponse resp)
+	throws ServletException, IOException{
+		
+		funcServlet(req, resp , "get");
+		
 	}
-
+	
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		funcServlet(request,response, "post");
+	protected void doPost(
+			HttpServletRequest req,
+			HttpServletResponse resp)
+	throws ServletException, IOException{
+		
+		funcServlet(req, resp , "post");
+		
 	}
-
-	@Override
-	public void funcServlet(HttpServletRequest request, HttpServletResponse response, String method) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		response.setCharacterEncoding("UTF-8");
-
-		String strMovePath = "/jsp/r06.jsp";
-
-		PrintWriter out = response.getWriter();
-
-  //スクリプトレット
-  //  Javaプログラムを書くところ
-//DB連携の変数定義
-		Connection con = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-
-		ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-
-		try {
-		//①Driverの読み込み
-			Class.forName("com.mysql.jdbc.Driver");
-
-			//②DB接続
-			con = DriverManager.getConnection("jdbc:mysql://localhost/2024jv32?useSSL=false", "root", "Passw0rd");
-			//SQL:alter user 'root'@' localhost' identified with mysql_native_password by 'Passw0rd';
-
-			//③SQL実行変数の作成
-			stmt = con.createStatement();
-
-			//④SQL文の作成
-			String strSQL = " select * from t_user ";
-
-			//⑤SQL
-			//stmt.executeQuery : select文
-			//stmt.executeUpdate : select文以外
-			rs = stmt.executeQuery(strSQL);
-
-			while(rs.next()){
-				System.out.println(rs.getString("f_name"));
-				System.out.println("<br/>");
-
-				HashMap<String, String> map = new HashMap<String, String>();
-
-				map.put("f_id",rs.getString("f_id"));
-				map.put("f_name",rs.getString("f_name"));
-				map.put("f_age",rs.getString("f_age"));
-				map.put("f_pass",rs.getString("f_pass"));
-
-				list.add(map);
+	
+	protected void funcServlet(
+			HttpServletRequest req,
+			HttpServletResponse resp,
+			String strKind)
+	throws ServletException, IOException{
+		
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		
+		//遷移先のファイル名
+		String strMovePath = "/jsp/r06_result.jsp";
+		
+		/***処理の開始********/
+			//DB連携の変数定義
+			Connection con = null;
+			Statement stmt = null;
+			ResultSet rs = null;
+			
+			//連想配列(HashMap)を要素とした配列
+			//	→ArrayListクラス(動的配列)
+			//		→<E>:Element(要素)
+			ArrayList<HashMap<String, String>> ary = new ArrayList<HashMap<String,String>>();
+			
+			//	↓	HashMapからBeansに変更
+			ArrayList<UserBean> aryBean = new ArrayList<UserBean>();
+			
+			try {
+				
+				//①Driverの読み込み
+				Class.forName("com.mysql.jdbc.Driver");
+	
+				//②DB接続
+				con = DriverManager.getConnection("jdbc:mysql://localhost/2024jv32?useSSL=false", "root", "Passw0rd");
+				//SQL:alter user 'root'@' localhost' identified with mysql_native_password by 'Passw0rd';
+	
+				//③SQL実行変数の作成
+				stmt = con.createStatement();
+	
+				//④SQL文の作成
+				String strSQL = " select * from t_user ";
+	
+				//⑤SQL
+				//stmt.executeQuery : select文
+				//stmt.executeUpdate : select文以外
+				rs = stmt.executeQuery(strSQL);
+	
+				while(rs.next()){
+					System.out.println(rs.getString("f_name"));
+					System.out.println("<br />");
+					
+					HashMap<String, String> map = new HashMap<String, String>();
+					map.put("f_id", rs.getString("f_id"));
+					map.put("f_name", rs.getString("f_name"));
+					map.put("f_age", rs.getString("f_age"));
+					map.put("f_password", rs.getString("f_password"));
+					
+					ary.add(map);
+					//	↓HashMapからBeansに変更
+					UserBean bean1 = new UserBean();
+					bean1.setId  (rs.getString("f_id")      );
+					bean1.setName(rs.getString("f_name")    );
+					bean1.setAge (rs.getInt("f_age")        );
+					bean1.setPass(rs.getString("f_password"));
+					aryBean.add(bean1);
+					bean1 = null;
 			}
 
-			//rsの解放
-			rs.close();
-			//stmtの解放
-			stmt.close();
-			//DB切断
-			con.close();
-		}catch (ClassNotFoundException err) {
-			err.printStackTrace();
-		}catch (SQLException err) {
-			err.printStackTrace();
+
+		}catch(ClassNotFoundException e) {
+			
+		}catch(SQLException e) {
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+			
 		}finally {
-			try {rs.close();}catch (SQLException err) {err.printStackTrace();}
-			try {stmt.close();}catch (SQLException err) {err.printStackTrace();}
-			try {con.close();}catch (SQLException err) {err.printStackTrace()}
+			//rsの解放
+			try{rs.close();}catch(Exception e) {}
+			//stmtの解放
+			try{stmt.close();}catch(Exception e) {}
+			//DB切断
+			try{con.close();}catch(Exception e) {}
+			
 		}
+			
+		//ServletからJSPへ値渡し
+		//Attribute(属性)
+		req.setAttribute("dataRows", ary);
+		req.setAttribute("beanAry", aryBean);
+		
+		
 
-		out.println("Servlet から実行("+method+")");
-		out.println("test");
-
-		RequestDispatcher dispatcher = request.getRequestDispatcher(strMovePath);
-		dispatcher.forward(request, response);
-
+		/***処理の終了********/
+		
+		PrintWriter out = resp.getWriter();
+		out.println("servelet("+strKind+")");
+		
+		//ServletからJSPへ遷移
+		RequestDispatcher dispatcher = 
+				req.getRequestDispatcher(strMovePath);
+		dispatcher.forward(req, resp);
+		
+		//String strData = (String)req.getAttribute("dataRows");
+		
+		
+		
+		
 	}
 }
--- class/tomcat/src/example/r06_result.jsp
+++ class/tomcat/src/example/r06_result.jsp
@@ -1,26 +1,45 @@
-<%@page import="java.util.HashMap" %>
-<%@page import="java.util.ArrayList" %>
-<%@page lnaguage="java" contentType="text/html; charset=UTF-9" %>
+<%@page import="myBean.UserBean"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.ArrayList"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>r06_result.jsp</title>
+<meta charset="UTF-8">
+<title>r06_result.jsp</title>
 <%
   String strPath = request.getContextPath();
 %>
 </head>
 <body>
-
+<h4>r06_result.jsp</h4>
 <%
-ArrayList<HashMap<String, String>> aryData = (ArrayList<HashMap<String, String>>) request.getAttribute("dataRows");
+	//Servletから値の受け取り
+	ArrayList<HashMap<String, String>> aryData =
+		(ArrayList<HashMap<String, String>>)
+			request.getAttribute("dataRows");
 
-if (aryData != null) {
-  for (int i = 0; i < aryData.size(); i++) {
-    HashMap<String,String> map = aryData(i);
-    out.println(aryData.get(i).get("f_name"));
-    out.println("<br>");
-  }
-}
+	if(aryData !=null){
+		for(HashMap<String,String> map : aryData){
+			out.println(map.get("f_name"));
+			out.println("<br />");
+		}
+	
+	}
+	
+	//Beanクラスのプログラム
+	ArrayList< UserBean > aryDataBean = (ArrayList< UserBean >) request.getAttribute("beanAry");
+	
+	if (aryDataBean != null){
+		out.println("*** Beanで出力 ***<br/>");
+		for( UserBean bean1 : aryDataBean ){
+			out.println( bean1.getName() );
+			out.println( "<br />" );
+		}
+	}
+
 %>
+
 </body>
 </html>

1fef2ac31fe6cdaf230ff1d018362695a81ea161
sikigasa <km3t1114@yahoo.co.jp>
2025-01-28 10:19:41 +0900

    add: 授業

---
+++ struts/src/main/java/snippet/Snippet.java
@@ -0,0 +1,6 @@
+package snippet;
+
+public class Snippet {
+	android: Unsupported Java Runtime: The Java version: 21, that is selected for the project is not supported by Gradle 6.7.The IDE will attempt to use Gradle 8.10 to gather the project information. Possible solutions: * Upgrade your Gradle version on your project * Select Java Runtime 15 (or below), on Build > Gradle Execution settings, to avoid this problem!
+}
+
